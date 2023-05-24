3e96fc7c77efd34ad3968c133062e8413d4fd53f
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 18:58:10 -0500

    Updated HomePage Hero component

--- components/HomePage/Hero.vue
+++ components/HomePage/Hero.vue
@@ -1,9 +1,22 @@
+<script lang="ts" setup>
+import { mdiXml } from '@mdi/js'
+
+const iconPath = ref<string>(mdiXml)
+</script>
+
 <template>
     <div class="hero min-h-screen bg-base-100">
-        <div class="hero-content text-center">
+        <div 
+            class="hero-content text-center" 
+            :style="{
+                position:'relative', 
+                top:'-50px'
+            }"
+        >
             <div class="max-w-lg">
+                <svg-icon class="mx-auto pb-8" type="mdi" :path="iconPath" :size="200" />
                 <h1 class="text-5xl font-bold">Hi I'm Fox, and I am a <br> 
-                    Full Stack Developer
+                    <span class="primary-highlight">Full Stack Developer</span>
                 </h1>
             </div>
         </div>
