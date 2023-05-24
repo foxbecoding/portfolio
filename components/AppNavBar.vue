e04c3a4bd14717b1b1f0097ff847b38449e2a4db
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 15:03:27 -0500

    Added AppNavBar component

---
+++ components/AppNavBar.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+
+interface Button {
+    id: number
+    class: string
+    label: string
+}
+
+const navActions = ref<Button[]>([
+    {id: 1, class: 'btn bg-neutral mr-4', label: 'Contact Me'},    
+    {id: 2, class: 'btn bg-neutral', label: 'My Resume'}    
+])
+
+</script>
+
+<template>
+    <div class="navbar bg-base-100 px-0">
+        <AppFrame>
+            <div class="flex-1">
+                <Logo :width="180" />
+            </div>
+            <div class="flex-none">
+                <button 
+                    v-for="(btn, i) in navActions" 
+                    :key="i" 
+                    :class="btn.class"
+                >
+                    {{ btn.label }}
+                </button>
+            </div>
+        </AppFrame>
+    </div>
+</template>
