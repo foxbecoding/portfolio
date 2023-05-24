c4485e6c655a41ef8201de7143dcabbb6b1d01db
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 15:33:04 -0500

    Updated HomePageHero component

--- components/HomePage/Hero.vue
+++ components/HomePage/Hero.vue
@@ -3,9 +3,8 @@
         <div class="hero-content flex-col lg:flex-row-reverse">
             <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
             <div>
-                <h1 class="text-5xl font-bold">Box Office News!</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <button class="btn btn-primary">Get Started</button>
+                <h1 class="text-5xl font-bold">Hi I'm Fox, and I am a <br> Full Stack Developer </h1>
+                
             </div>
         </div>
     </div>
