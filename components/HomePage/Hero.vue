eb1140b38aed060934568765bf39ca727aa662e3
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 15:35:57 -0500

    Update HomePage Hero component

--- components/HomePage/Hero.vue
+++ components/HomePage/Hero.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="hero min-h-screen bg-base-100">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-            <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-                <h1 class="text-5xl font-bold">Hi I'm Fox, and I am a <br> Full Stack Developer </h1>
-                
+        <div class="hero-content text-center">
+            <div class="max-w-lg">
+                <h1 class="text-5xl font-bold">Hi I'm Fox, and I am a <br> 
+                    Full Stack Developer
+                </h1>
             </div>
         </div>
     </div>
