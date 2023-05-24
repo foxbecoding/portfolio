cec8d58a5d25934003d950ab40d5e194143d9c7c
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 13:23:49 -0500

    Added Logo component

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+const props = defineProps({
+    width: Number
+})
+
+</script>
+
+<template>
+    <div>
+        <img src="logo.png" :width="props.width ? props.width : 150" />
+    </div>
+</template>
