409bd060916ead4d575d4a717620911020e34cef
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 14:07:31 -0500

    Added AppWrap component

---
+++ components/AppWrap.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="application__wrap">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.application__wrap {
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+    max-width: 100%;
+    min-height: 100vh;
+    min-height: 100dvh;
+    position: relative;
+}
+</style>
