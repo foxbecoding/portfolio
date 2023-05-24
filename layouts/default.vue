43a63653d75f3945dd66ecd59eda11f0ba7920c8
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 15:05:51 -0500

    Updated default layout with AppNavBar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,7 @@
 
 <template>
     <AppWrap>
-        <div class="navbar bg-base-100 px-0">
-            <AppFrame>
-                <div class="flex-1">
-                    <Logo :width="180" />
-                </div>
-                <div class="flex-none">
-                    <button class="btn bg-neutral mr-4">Contact Me</button>
-                    <button class="btn bg-neutral">My Resume</button>
-                </div>
-            </AppFrame>
-        </div>
+        <AppNavBar />
         <main class="app-main">
             <slot />
         </main>
