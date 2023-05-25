7716d525b28af7249f52d1bc398888eb40bd0519
FoxBeCoding <fox@foxbecoding.com>
2023-05-25 12:13:23 -0500

    changed projects container from flex to grid

--- components/HomePage/Projects.vue
+++ components/HomePage/Projects.vue
@@ -39,37 +39,39 @@ const projects = ref<Project[]>([
         <div class="text-center">
             <h1 class="text-5xl font-bold">My Projects</h1>
         </div>
-        <div class="flex justify-center gap-4 mt-8">
-            <div
-                v-for="(project, i) in projects" 
-                :key="i" 
-                class="card w-96 bg-base-100 image-full"
-            >
-                <figure>
-                    <img :src="project.image.src" :alt="project.image.alt" />
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title">{{ project.title }}</h2>
-                    <p>Tech Stack: <br>{{ project.techStack }}</p>
-                    <div class="card-actions justify-end">
-                        <a 
-                            class="btn btn-sm glass"
-                            target="_blank" 
-                            :href="project.liveSiteLink"
-                        >
-                            Live Site
-                        </a>
-                        <a 
-                            class="btn btn-accent btn-sm" 
-                            target="_blank"
-                            :href="project.githubLink"
-                        >
-                            <svg-icon class="mr-2" type="mdi" :path="iconPath" :size="24" />
-                            Github
-                        </a>
+        <AppFrame>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+                <div
+                    v-for="(project, i) in projects" 
+                    :key="i" 
+                    class="card bg-base-100 image-full"
+                >
+                    <figure>
+                        <img :src="project.image.src" :alt="project.image.alt" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">{{ project.title }}</h2>
+                        <p>Tech Stack: <br>{{ project.techStack }}</p>
+                        <div class="card-actions justify-end">
+                            <a 
+                                class="btn btn-sm glass"
+                                target="_blank" 
+                                :href="project.liveSiteLink"
+                            >
+                                Live Site
+                            </a>
+                            <a 
+                                class="btn btn-accent btn-sm" 
+                                target="_blank"
+                                :href="project.githubLink"
+                            >
+                                <svg-icon class="mr-2" type="mdi" :path="iconPath" :size="24" />
+                                Github
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>  
+        </AppFrame>  
     </div>
 </template>
