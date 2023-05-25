471f5ed45098beca6c02338e0679eea09b9024dd
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 21:29:22 -0500

    Update HomePageSocial component

--- components/HomePage/Social.vue
+++ components/HomePage/Social.vue
@@ -1,5 +1,5 @@
 <template>  
-    <div class="py-4">
+    <div class="py-8">
         <div class="text-center">
             <h1 class="text-5xl font-bold">Find me on LinkedIn</h1>
         </div>  
