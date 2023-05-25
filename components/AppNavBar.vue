4b369da8d0557c2ea60e0b962cec3a82b74f0fae
FoxBeCoding <fox@foxbecoding.com>
2023-05-25 11:12:08 -0500

    updated mobile breakpoints for navBarActions

--- components/AppNavBar.vue
+++ components/AppNavBar.vue
@@ -7,7 +7,7 @@ interface NavBarAction {
 }
 
 const navBarActions = ref<NavBarAction[]>([
-    {id: 1, class: 'btn btn-sm bg-neutral', label: 'Contact Me'},    
+    {id: 1, class: 'btn btn-sm bg-neutral glass', label: 'Contact Me'},    
     {id: 2, class: 'btn btn-sm bg-neutral', label: 'My Resume'}    
 ])
 
@@ -19,7 +19,7 @@ const navBarActions = ref<NavBarAction[]>([
             <div class="flex-1">
                 <Logo :width="180" />
             </div>
-            <div class="flex-none space-x-4">
+            <div class="invisible sm:visible flex-none space-x-4">
                 <button 
                     v-for="(nba, i) in navBarActions" 
                     :key="i" 
