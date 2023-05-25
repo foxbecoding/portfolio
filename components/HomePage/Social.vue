4e34929a5293f21319690f8de0285f22f73ec725
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 20:44:29 -0500

    Added new HomePageSocial component

---
+++ components/HomePage/Social.vue
@@ -0,0 +1,7 @@
+<template>  
+    <div class="py-4">
+        <div class="text-center">
+            <h1 class="text-5xl font-bold">Find me on LinkedIn</h1>
+        </div>  
+    </div>
+</template>
