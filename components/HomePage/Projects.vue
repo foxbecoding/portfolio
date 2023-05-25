85c356a90c563124744cea129d032452ec13b59e
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 20:40:44 -0500

    Updated HomePageProjects component

--- components/HomePage/Projects.vue
+++ components/HomePage/Projects.vue
@@ -1,5 +1,52 @@
-<template>
-    <div class="text-center">
-        <h1 class="text-5xl font-bold">My Projects</h1>
+<script lang="ts" setup>
+interface Project {
+    id: number
+    title: string
+    techStack: string
+    image: { src: string, alt: string }
+}
+
+const projects = ref<Project[]>([
+    { 
+        id: 1,
+        title: 'Portfolio Site',  
+        techStack: 'Nuxt3+TailWind CSS+DaisyUI+TypeScript',
+        image: { src: '/images/foxbecoding-sq.png', alt: 'FoxBeCoding' }
+    },
+
+    { 
+        id: 2,
+        title: 'EasyBeatz',  
+        techStack: 'Nuxt3+Vuetify+TypeScript+Python+Django Rest',
+        image: { src: '/images/easybeatz-sq.png', alt: 'FoxBeCEasyBeatzoding' }
+    }
+])
+
+</script>
+
+<template>  
+    <div class="py-4">
+        <div class="text-center">
+            <h1 class="text-5xl font-bold">My Projects</h1>
+        </div>
+        <div class="flex justify-center gap-4 mt-8">
+            <div
+                v-for="(project, i) in projects" 
+                :key="i" 
+                class="card w-96 bg-base-100 image-full"
+            >
+                <figure>
+                    <img :src="project.image.src" :alt="project.image.alt" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">{{ project.title }}</h2>
+                    <p>Tech Stack: <br>{{ project.techStack }}</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-outline btn-sm">Live Site</button>
+                        <button class="btn btn-accent btn-sm">GitHub</button>
+                    </div>
+                </div>
+            </div>
+        </div>  
     </div>
 </template>
