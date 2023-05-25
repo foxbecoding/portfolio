a29c3121cae426bdabe224ff2395003b2597a4c4
FoxBeCoding <fox@foxbecoding.com>
2023-05-24 22:08:19 -0500

    Updated HomePageSocial component

--- components/HomePage/Social.vue
+++ components/HomePage/Social.vue
@@ -1,7 +1,23 @@
+<script lang="ts" setup>
+import { mdiLinkedin } from '@mdi/js'
+
+const iconPath = ref<string>(mdiLinkedin)
+</script>
+
 <template>  
     <div class="py-8">
         <div class="text-center">
             <h1 class="text-5xl font-bold">Find me on LinkedIn</h1>
-        </div>  
+        </div>
+        <div class="text-center my-4">
+            <div class="avatar">
+                <div class="w-24 mask mask-squircle bg-neutral">
+                    <svg-icon  type="mdi" :path="iconPath" :size="98" />
+                </div>
+            </div>
+            <h2 class="text-4xl font-bold"> 
+                <span class="primary-highlight">@foxbecoding</span>
+            </h2>  
+        </div>
     </div>
 </template>
