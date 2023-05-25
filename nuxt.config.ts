0935d1ced25bfaad130669b36a0bf1be4c184abd
FoxBeCoding <fox@foxbecoding.com>
2023-05-25 16:50:15 -0500

    added title and description to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,21 @@ export default defineNuxtConfig({
             BASE_URL: process.env.BASE_URL
         }
     },
+    app: {
+        head: { 
+            title: 'FoxBeCoding portfolio',
+            meta: [
+                { 
+                    hid: 'description', 
+                    property: 'description', 
+                    content: "Fox portfolio site: Explore projects created by Fox."
+                },
+            ],
+            htmlAttrs: {
+                lang: 'en'
+            }  
+        }
+    },
     css: [
         '@mdi/font/css/materialdesignicons.min.css',
         '@fortawesome/fontawesome-free/css/all.min.css',
